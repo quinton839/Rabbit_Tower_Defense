264e13fca64eb34ab0e3eaf58d2a5ef170f04a27
Quinton <quinton841@gmail.com>
2019-02-10 20:45:00 +0800

    health

---
+++ Rabbit_tower_defense/Assets/Scripts/HealthController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public float maxHealth = 10f;
+    public float health = 10f;
+    public Transform valueBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 theScale = valueBar.localScale;
+        if (health > maxHealth)
+            health = maxHealth;
+        else if (health <= 0)
+            health = 0;
+
+        theScale.x = health / maxHealth;
+        valueBar.localScale = theScale;
+    }
+}
--- Rabbit_tower_defense/Assets/Scripts/WolfControllerScripts.cs
+++ Rabbit_tower_defense/Assets/Scripts/WolfControllerScripts.cs
@@ -7,12 +7,14 @@ public class WolfControllerScripts : MonoBehaviour
     Animator anim;
     Rigidbody2D rigi;
     float move = 0;
+    bool gotTarget = false;
     public float moveSpeed = 1f;
     public bool facingRight = true;
+    public bool canAttack = true;
     public float attackSpeed = 1f;
-    public bool gotTarget = false;
     public Transform targetCheck;
     public LayerMask whatIsTarget;
+    public string whatIsLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +22,27 @@ public class WolfControllerScripts : MonoBehaviour
         anim = GetComponent<Animator>();
         rigi = GetComponent<Rigidbody2D>();
         anim.SetFloat("Attack Speed", attackSpeed);
+
+        SpriteRenderer[] spriteList = GetComponentsInChildren<SpriteRenderer>();
+        for (int i = 0; i < spriteList.Length; i++)
+        {
+            spriteList[i].sortingLayerName = whatIsLayer;
+        }
+
+        if (!facingRight)
+            flip();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        gotTarget = Physics2D.OverlapBox(targetCheck.position, new Vector2(0.01f, 20), 0f, whatIsTarget);
-        anim.SetBool("Target", gotTarget);
+        gotTarget = Physics2D.OverlapBox(targetCheck.position, new Vector2(0.001f, 4), 0f, whatIsTarget);
+        anim.SetBool("Target", canAttack && gotTarget);
         if (gotTarget)
             move = 0;
         else
-            move = moveSpeed;
+            move = facingRight ? moveSpeed : -moveSpeed;
 
         anim.SetFloat("Speed", Mathf.Abs(move));
         rigi.velocity = new Vector2(move, rigi.velocity.y);
@@ -40,9 +52,23 @@ public class WolfControllerScripts : MonoBehaviour
     {
 
     }
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject target = other.gameObject.transform.parent.gameObject; ;
+        int otherlayer = target.layer;
+        bool matched = whatIsTarget % (1 << (otherlayer + 1)) / (1 << otherlayer) == 1;
+        if (matched)
+        {
+            if (target.GetComponentInChildren<HealthController>())
+                target.GetComponentInChildren<HealthController>().health -= 1f;
+        }
+    }
+
     void flip()
     {
-        facingRight = !facingRight;
+        // facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
